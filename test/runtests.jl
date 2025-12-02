a8b6f1b5eb3897c32500480212c410aa44fe97d3
Adrian Salceanu <essenciary@users.noreply.github.com>
2025-12-02 17:44:03 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,43 @@ cd(@__DIR__)
 
 using Pkg
 
-using Test, TestSetExtensions, SafeTestsets
+using Test, TestSetExtensions, SafeTestsets, Logging
 using SearchLight
+
+macro includetests(testarg...)
+  if length(testarg) == 0
+      tests = []
+  elseif length(testarg) == 1
+      tests = testarg[1]
+  else
+      error("@includetests takes zero or one argument")
+  end
+
+  rootfile = "$(__source__.file)"
+  mod = __module__
+
+  quote
+      tests = $tests
+      rootfile = $rootfile
+
+      if length(tests) == 0
+          tests = readdir(dirname(rootfile))
+          tests = filter(f->endswith(f, ".jl") && f!= basename(rootfile), tests)
+      else
+          tests = map(f->string(f, ".jl"), tests)
+      end
+
+      println();
+
+      for test in tests
+          print(splitext(test)[1], ": ")
+          Base.include($mod, test)
+          println()
+      end
+  end
+end
+
+Logging.global_logger(NullLogger())
 
 @testset ExtendedTestSet "SearchLight tests" begin
   @includetests ARGS
