d75f75432900aec40d19078375b6167dd7012452
hhaensel <helmut.haensel@gmx.com>
2026-04-30 09:34:36 +0200

    switch from JSON3 to JSON

--- serializers/JsonSerializer.jl
+++ serializers/JsonSerializer.jl
@@ -1,16 +1,16 @@
 module JsonSerializer
 
-using JSON3
+using JSON
 
 using SearchLight
 using SearchLight.Serializer
 
 function SearchLight.Serializer.serialize(value) :: String
-  JSON3.write(value)
+  JSON.json(value)
 end
 
-function SearchLight.Serializer.deserialize(::Type{T}, value)::T where {T}
-  JSON3.read(value, T)
+function SearchLight.Serializer.deserialize(::Type{T}, value; dicttype = Dict)::T where {T}
+  JSON.parse(value, T; dicttype)
 end
 
 end
